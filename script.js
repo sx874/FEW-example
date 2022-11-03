68c1347be4bbf9a38b848f8a036eae531f381584
sx874 <93100053+sx874@users.noreply.github.com>
2022-11-03 17:22:05 -0400

    red border

---
+++ script.js
@@ -0,0 +1,43 @@
+const bttn = document.getElementById("submit");
+
+bttn.addEventListener('click', () => {
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const email = document.getElementById("email").value;
+    const check = document.getElementById("check").value;
+
+    if (fname !== '' && lname !== '' && email !== '' && check === true) {
+        document.getElementById("submit").classList.add("disappear");
+        document.getElementById("loader").classList.add("appear");
+        document.getElementById("form").classList.add("bg"); //testing
+        setTimeout(() => {
+            document.getElementById("loader").classList.remove("appear")
+            document.getElementById("thanks").innerText = 'thanks for signing up!';
+        }, 3000);
+    } else {};
+    if (fname === '') {
+        document.getElementById("fname").classList.add("redBorder");
+    } else {
+        document.getElementById("fname").classList.remove("redBorder");
+    };
+    if (lname === '') {
+        document.getElementById("lname").classList.add("redBorder");
+    } else {
+        document.getElementById("lname").classList.remove("redBorder");
+    };
+    if (email === '') {
+        document.getElementById("email").classList.add("redBorder");
+    } else {
+        document.getElementById("email").classList.remove("redBorder");
+    };
+    if (check !== true) {
+        document.getElementById("check").classList.add("redBorder");
+    } else {
+        document.getElementById("check").classList.remove("redBorder");
+    }
+})
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
